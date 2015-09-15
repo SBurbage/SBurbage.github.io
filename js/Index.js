dab12037ed4d405650eade4ed92fe4d12d391ff1
SBurbage <sam.burbage@d2n2lep.org>
2015-09-15 15:48:31 +0100

    Created Script for Status Box

--- js/Index.js
+++ js/Index.js
@@ -1,1 +1,8 @@
-
+var main = function() {
+  $('#views').click(function() {
+    var post = $('.boxy').val();
+    $('<li>').text(post).prependTo('.views').addClass('view');
+      $('.boxy').val('');
+  });
+};
+$(document).ready(main);

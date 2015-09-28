ac0273c4be173072f22552faff94f92200edeb87
SBurbage <sam.burbage@d2n2lep.org>
2015-09-28 10:33:03 +0100

    Removed Blob Box JS

--- js/Index.js
+++ js/Index.js
@@ -1,9 +1,1 @@
-var main = function () {
-    $('#fill').click(function () {
-        $('<div>').appendTo('#canvas').addClass('blob');
-    });
-    $('#clear').click(function(){
-        $('#canvas').empty();
-    });
-};
-$(document).ready(main);
+
